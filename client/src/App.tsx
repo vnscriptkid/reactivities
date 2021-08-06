96dd75b6e10df3d7a921b0015d5830429fc7f340
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-06 15:31:21 +0700

    [Client] Integrate React with SemanticUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
 
@@ -14,11 +15,12 @@ function App() {
   
   return (
     <div>
-      <ul>
+      <Header as="h2" icon="users" content="Reactivities"/>
+      <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>{activity.title}</li>
+          <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
-      </ul>
+      </List>
     </div>
   );
 }
