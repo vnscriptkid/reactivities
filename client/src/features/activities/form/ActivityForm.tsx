d28f0038628bf5fcf3950ac6d2158d7897bd5fea
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-08 14:04:40 +0700

    [Client] Refactor form to use formik

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import { observer } from 'mobx-react-lite';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, Segment } from 'semantic-ui-react';
 
 import { Activity } from '../../../app/models/Activity';
 import { useStore } from '../../../app/stores/store';
@@ -8,6 +8,7 @@ import { useEffect } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import Loading from '../../../app/layout/Loading';
 import { v4 as uuid } from 'uuid';
+import { Field, Form, Formik } from 'formik';
 
 function ActivityForm() {
     
@@ -38,7 +39,7 @@ function ActivityForm() {
         }
     }, [id, loadActivity]);
 
-    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = event.target;
         setActivity({ ...activity, [name]: value });
     }
@@ -58,17 +59,21 @@ function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Form onSubmit={handleSubmit}>
-                <Form.Input value={activity.title} onChange={onChangeInput} name='title' placeholder='Title' />
-                <Form.TextArea value={activity.description} onChange={onChangeInput} name='description' placeholder='Description' />
-                <Form.Input value={activity.category} onChange={onChangeInput} name='category' placeholder='Category' />
-                <Form.Input type='date' value={activity.date} onChange={onChangeInput} name='date' placeholder='Date' />
-                <Form.Input value={activity.city} onChange={onChangeInput} name='city' placeholder='City' />
-                <Form.Input value={activity.venue} onChange={onChangeInput} name='venue' placeholder='Venue' />
+            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+                {({handleSubmit}) => (
+                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                        <Field name='title' placeholder='Title' />
+                        <Field name='description' placeholder='Description' />
+                        <Field name='category' placeholder='Category' />
+                        <Field type='date' name='date' placeholder='Date' />
+                        <Field name='city' placeholder='City' />
+                        <Field name='venue' placeholder='Venue' />
 
-                <Button loading={loading} disabled={loading} floated='right' positive type='submit' content='Submit' />
-                <Button as={Link} to={id ? `/activities/${id}` : '/activities'} floated='right' content='Cancel' />
-            </Form>
+                        <Button loading={loading} disabled={loading} floated='right' positive type='submit' content='Submit' />
+                        <Button as={Link} to={id ? `/activities/${id}` : '/activities'} floated='right' content='Cancel' />
+                    </Form>
+                )}
+            </Formik>
         </Segment>
     );
 }
