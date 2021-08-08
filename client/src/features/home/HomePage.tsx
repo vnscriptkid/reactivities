e4455cdf11b0b484d379225183fe981e3e46d78e
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-08 10:44:13 +0700

    [Client] Homepage has no navbar

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 function HomePage() {
     return (
         <Container style={{ marginTop: '7em' }}>
             <h1>Home Page</h1>
+            <h2>Go to <Link to='/activities'>Activities</Link></h2>
         </Container>
     );
 }
