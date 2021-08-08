d779c740e66aad8d5be39387d77e64b14f4bfd70
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-08 14:30:35 +0700

    [Client] Add form validations, reuse form input

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import { observer } from 'mobx-react-lite';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 
 import { Activity } from '../../../app/models/Activity';
 import { useStore } from '../../../app/stores/store';
@@ -8,7 +8,9 @@ import { useEffect } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import Loading from '../../../app/layout/Loading';
 import { v4 as uuid } from 'uuid';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
 
 function ActivityForm() {
     
@@ -55,19 +57,33 @@ function ActivityForm() {
         }
     }
 
+    
     if (initialLoading) return <Loading />
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('Title is required'),
+        description: Yup.string().required('Description is required'),
+        category: Yup.string().required('Category is required'),
+        date: Yup.string().required('Date is required'),
+        city: Yup.string().required('City is required'),
+        venue: Yup.string().required('Venue is required'),
+    });
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+            <Formik 
+                validationSchema={validationSchema}
+                enableReinitialize 
+                initialValues={activity} 
+                onSubmit={values => console.log(values)}>
                 {({handleSubmit}) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <Field name='title' placeholder='Title' />
-                        <Field name='description' placeholder='Description' />
-                        <Field name='category' placeholder='Category' />
-                        <Field type='date' name='date' placeholder='Date' />
-                        <Field name='city' placeholder='City' />
-                        <Field name='venue' placeholder='Venue' />
+                        <MyTextInput name='title' placeholder='Title' />
+                        <MyTextInput name='description' placeholder='Description' />
+                        <MyTextInput name='category' placeholder='Category' />
+                        <MyTextInput name='date' placeholder='Date' />
+                        <MyTextInput name='city' placeholder='City' />
+                        <MyTextInput name='venue' placeholder='Venue' />
 
                         <Button loading={loading} disabled={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to={id ? `/activities/${id}` : '/activities'} floated='right' content='Cancel' />
