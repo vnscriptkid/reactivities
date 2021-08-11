ad8e2d6c8510e53103452bdffac980c26dac6904
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-11 23:38:18 +0700

    [Client] Install React-dropzone, attach to widget

---
+++ client/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,23 @@
+import {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+function PhotoWidgetDropzone() {
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        console.log(acceptedFiles);
+      }, [])
+      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    
+      return (
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+              <p>Drop the files here ...</p> :
+              <p>Drag 'n' drop some files here, or click to select files</p>
+          }
+        </div>
+      )
+}
+
+export default PhotoWidgetDropzone;
