be8d386c8b4824ee2aed35e2d4749288c90140e1
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-11 22:35:28 +0700

    [Client] Show user's photos on profile page

--- client/src/features/profiles/ProfileContent.tsx
+++ client/src/features/profiles/ProfileContent.tsx
@@ -1,15 +1,16 @@
 import { observer } from 'mobx-react-lite';
 import { Tab } from 'semantic-ui-react';
 import { Profile } from '../../app/models/Profile';
+import ProfilePhotos from './ProfilePhotos';
 
 interface Props {
     profile: Profile;
 }
 
-function ProfileContent(props: Props) {
+function ProfileContent({ profile }: Props) {
     const panes = [
         { menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane> },
-        { menuItem: 'Photos', render: () => <Tab.Pane>Photos Content</Tab.Pane> },
+        { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile}/> },
         { menuItem: 'Event', render: () => <Tab.Pane>Event Content</Tab.Pane> },
         { menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane> },
         { menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane> },
