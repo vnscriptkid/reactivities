bc0590c9d3d3f1ce1be4b74f3ebbfe1a3578db9f
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-22 17:09:33 +0700

    [Client] Guard private routes

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,22 +1,22 @@
-import { Container } from 'semantic-ui-react';
-import NavBar from './layout/NavBar';
-import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
-import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
-import HomePage from '../features/home/HomePage';
-import ActivityForm from '../features/activities/form/ActivityForm';
-import ActivityDetails from '../features/activities/details/ActivityDetails';
-import LoginForm from '../features/users/LoginForm';
-import { useStore } from './stores/store';
-import { useEffect } from 'react';
-import Loading from './layout/Loading';
-import ModalContainer from './common/modals/ModalContainer';
-import ProfilePage from '../features/profiles/ProfilePage';
+import { Container } from "semantic-ui-react";
+import NavBar from "./layout/NavBar";
+import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
+import { observer } from "mobx-react-lite";
+import { Route, useLocation } from "react-router-dom";
+import HomePage from "../features/home/HomePage";
+import ActivityForm from "../features/activities/form/ActivityForm";
+import ActivityDetails from "../features/activities/details/ActivityDetails";
+import LoginForm from "../features/users/LoginForm";
+import { useStore } from "./stores/store";
+import { useEffect } from "react";
+import Loading from "./layout/Loading";
+import ModalContainer from "./common/modals/ModalContainer";
+import ProfilePage from "../features/profiles/ProfilePage";
+import PrivateRoute from "./layout/PrivateRoute";
 
 function App() {
-
-  const {key} = useLocation();
-  const {commonStore, userStore} = useStore();
+  const { key } = useLocation();
+  const { commonStore, userStore } = useStore();
 
   useEffect(() => {
     if (commonStore.token) {
@@ -26,24 +26,40 @@ function App() {
     }
   }, [commonStore, userStore]);
 
-  if (!commonStore.appLoaded) return <Loading content='App loading...' />
+  if (!commonStore.appLoaded) return <Loading content="App loading..." />;
 
   return (
     <>
       <ModalContainer />
-      <Route exact path='/' component={HomePage} />
-      <Route path='/(.+)' render={() => (
-        <>
-          <NavBar />
-          <Container style={{marginTop: '7em'}}>
-            <Route exact path='/activities' component={ActivityDashboard} />
-            <Route path='/activities/:id' component={ActivityDetails} />
-            <Route path='/profiles/:username' component={ProfilePage} />
-            <Route key={key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-            <Route path='/login' component={LoginForm} />
-          </Container>
-        </>
-      )}/>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path="/(.+)"
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <PrivateRoute
+                exact
+                path="/activities"
+                component={ActivityDashboard}
+              />
+              <PrivateRoute
+                path="/activities/:id"
+                component={ActivityDetails}
+              />
+              <PrivateRoute
+                path="/profiles/:username"
+                component={ProfilePage}
+              />
+              <PrivateRoute
+                key={key}
+                path={["/createActivity", "/manage/:id"]}
+                component={ActivityForm}
+              />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
